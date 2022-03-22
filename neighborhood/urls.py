f84556251a66df120323f9af674153a75e5e5818
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-03-22 18:21:37 +0300

    configurations

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('neighborhood_App.urls')),
+    path(r'^accounts/', include('registration.backends.simple.urls')),
+    # path(r'logout/', views.LogoutView.as_view(), {"next_page": 'accounts/signup'}),
+    path(r'logout/', views.logout_then_login),
+    
 ]
+
